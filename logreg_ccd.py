36bbf4d24c68f534c5794531d0d6e3be7af33140
alicja <alicja.charuza@gmail.com>
2025-03-29 11:32:15 +0100

    add docstrings

--- logreg_ccd.py
+++ logreg_ccd.py
@@ -11,6 +11,19 @@ from sklearn.metrics import accuracy_score
 
 
 def evaluate_model(y_true, y_pred, y_scores, metric="balanced_accuracy"):
+    '''
+    Evaluates predictions using the specified metric. Supported metrics are:
+    ['recall', 'accuracy', 'precision', 'f1', 'balanced_accuracy', 'auc_roc', 'auc_pr']
+    
+    Args:
+        y_true (array-like): True labels.
+        y_pred (array-like): Predicted labels.
+        y_scores (array-like): Predicted probabilities.
+        metric (str): The metric to evaluate. Default is "balanced_accuracy".
+
+    Returns:
+        float: The computed metric value.
+    '''
     if metric == 'recall':
         return recall_score(y_true, y_pred)
     elif metric == 'accuracy':
@@ -31,6 +44,19 @@ def evaluate_model(y_true, y_pred, y_scores, metric="balanced_accuracy"):
 
 
 class LogRegCCD:
+    '''
+    Logistic Regression using Coordinate Descent with L1 regularization.
+    
+    Attributes:
+        tol (float): Tolerance for convergence.
+        max_iter (int): Maximum number of iterations.
+        best_lambda (float): Best lambda value found during training.
+        best_beta (array-like): Coefficients corresponding to the best lambda.
+        train_mean (float): Mean of the training data.
+        train_std (float): Standard deviation of the training data.
+    '''
+
+
     def __init__(self, tol=1e-6, max_iter=100):
         self.tol = tol
         self.max_iter = max_iter
@@ -41,11 +67,27 @@ class LogRegCCD:
         
 
     def S(self, z, gamma):
+        '''
+        Soft-thresholding operator for L1 regularization.
+        
+        Args:
+            z (float): Input value.
+            gamma (float): Regularization parameter.
+        
+        Returns:
+            float: Soft-thresholded value.
+        '''
         return np.sign(z) * max(np.abs(z) - gamma, 0)
 
     def prepare_train_data(self, X_train):
         '''
-        Standardize the data and add a column of ones for the intercept.
+        Standardize the training data and add a column of ones for the intercept.
+
+        Args:
+            X_train (array-like): Training features.
+
+        Returns:
+            array-like: Standardized training features with intercept.
         '''
         self.train_mean = np.mean(X_train, axis=0)
         self.train_std = np.std(X_train, axis=0) + 1e-8
@@ -54,11 +96,31 @@ class LogRegCCD:
         return X_train_ready
 
     def prepare_test_data(self, X_test):
-        # Dodałem standaryzacje dla zbiorku testowego na bazie zbiorku treningowego
+        '''
+        Standardize the test data using the training mean and std, and add a column of ones for the intercept.
+        
+        Args:
+            X_test (array-like): Test features.
+            
+        Returns:
+            array-like: Standardized test features with intercept.
+        '''
         X_test_stand = (X_test - self.train_mean) / self.train_std
         return np.hstack([np.ones((X_test.shape[0], 1)), X_test])
 
     def coordinate_descent(self, X, y, lambd, beta):
+        '''
+        Performs coordinate descent to update the coefficients.
+        
+        Args:
+            X (array-like): Feature matrix.
+            y (array-like): Target variable.
+            lambd (float): Regularization parameter.
+            beta (array-like): Current coefficients.
+        
+        Returns:
+            array-like: Updated coefficients.
+        '''
         N, p = X.shape
         beta_new = beta.copy()
         for j in range(1, p):
@@ -68,8 +130,19 @@ class LogRegCCD:
         return beta_new
 
     def fit(self, X_train, y_train, X_valid, y_valid, lambdas, metric="balanced_accuracy"):
-        # I added the option to fit lambda on validation set (it's in the description of the project),
-        # but in the example experiment I used the same set for training and validation
+        '''
+        Fits the model using coordinate descent on different lambda values. It chooses the best lambda based on the specified metric.
+        Supported metrics are:
+        ['recall', 'accuracy', 'precision', 'f1', 'balanced_accuracy', 'auc_roc', 'auc_pr']
+
+        Args:
+            X_train (array-like): Training features.
+            y_train (array-like): Training labels.
+            X_valid (array-like): Validation features.
+            y_valid (array-like): Validation labels.
+            lambdas (list): List of lambda values to evaluate.
+            metric (str): The metric to evaluate. Default is "balanced_accuracy".
+        '''
         X_train = self.prepare_train_data(X_train)
         X_valid = self.prepare_test_data(X_valid)
         N, p = X_train.shape
@@ -97,6 +170,17 @@ class LogRegCCD:
 
 
     def validate(self, X_valid, y_valid, metric="balanced_accuracy"):
+        '''
+        Calculates the specified metric for the validation set using the best beta found during training.
+        
+        Args:
+            X_valid (array-like): Validation features.
+            y_valid (array-like): Validation labels.
+            metric (str): The metric to evaluate. Default is "balanced_accuracy".
+        
+        Returns:
+            float: The computed metric value.
+        '''
         if self.best_beta is None:
             raise ValueError("Model is not trained yet")
 
@@ -106,11 +190,32 @@ class LogRegCCD:
         return evaluate_model(y_valid, preds, probs, metric)
 
     def predict_proba(self, X_test):
+        '''
+        Predicts probabilities for the test set using the best beta found during training.
+        
+        Args:
+            X_test (array-like): Test features.
+
+        Returns:
+            array-like: Predicted probabilities for the test set.
+        '''
         if self.best_beta is None:
             raise ValueError("Model is not trained yet")
-        return 1 / (1 + np.exp(-X_test @ self.best_beta))
+        return expit(X_test @ self.best_beta)
 
     def plot(self, X_train, y_train, X_valid, y_valid, lambdas, metric="balanced_accuracy"):
+        '''
+        Plots metric vs lambda values for the given lambdas and metric. Supported metrics are:
+        ['recall', 'accuracy', 'precision', 'f1', 'balanced_accuracy', 'auc_roc', 'auc_pr']
+        
+        Args:
+            X_train (array-like): Training features.
+            y_train (array-like): Training labels.
+            X_valid (array-like): Validation features.
+            y_valid (array-like): Validation labels.
+            lambdas (list): List of lambda values to evaluate.
+            metric (str): The metric to evaluate. Default is "balanced_accuracy".
+        '''
         scores = []
         for lambd in lambdas:
             self.fit(X_train, y_train, X_valid, y_valid, [lambd])
@@ -123,6 +228,15 @@ class LogRegCCD:
         plt.show()
 
     def plot_coefficients(self, lambdas, X_train, y_train, metric="balanced_accuracy"):
+        '''
+        Plots values of coefficients for different lambda values.
+        
+        Args:
+            lambdas (list): List of lambda values to evaluate.
+            X_train (array-like): Training features.
+            y_train (array-like): Training labels.
+            metric (str): The metric to evaluate. Default is "balanced_accuracy".
+        '''
         coefficients = []
         for lambd in lambdas:
             self.fit(X_train, y_train, X_train, y_train, [lambd], metric=metric)
